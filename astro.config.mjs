1d487e6e991ab491441e6c5eabefd764ed588a74
Haris Rafiq <mrafi0015@gmail.com>
2026-03-10 02:23:25 +0500

    updated domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://harisrafiq.github.io',
+  site: 'https://thinkingcats.com',
   integrations: [mdx(), sitemap()],
-  base: '/thinkingcats-landing', // e.g., '/my-astro-site/'
 
   vite: {
     plugins: [tailwindcss()],
